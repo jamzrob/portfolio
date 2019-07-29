eab4b7f6cd367296fe8fedd2ffdfd4b218873387
James Raubenheimer <raubenheimerjames@gmail.com>
2019-07-30 00:42:10 +0100

    Initial changes, colors, taking out medium posts, and general simplications

--- colors.js
+++ colors.js
@@ -2,11 +2,11 @@ module.exports = {
   background: '#FFFFFF',
   backgroundDark: '#f0e6f6',
 
-  primary: '#7c37ad',
-  primaryLight: '#ae66df',
-  primaryDark: '#4b007d',
+  primary: '#73877B',
+  primaryLight: '#A4CE85',
+  primaryDark: '#839788',
 
-  secondary: '#ff4081',
-  secondaryLight: '#ff79b0',
-  secondaryDark: '#c60055',
+  secondary: '#E5D1D0',
+  secondaryLight: '#F5E4D7',
+  secondaryDark: '#BCACAB',
 };
